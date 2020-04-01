bf35965853dfa3c1f4cfbafdbc30e972f5bce4f8
Polina <45530286+polinch@users.noreply.github.com>
2020-04-01 20:23:58 +0300

    v 1.0

---
+++ client/client/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        TcpClient client = new TcpClient();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string host = textBox1.Text;
+            int port;
+            if (host.Equals(""))
+            {
+                MessageBox.Show("Host is empty", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            try
+            {
+                port = Int32.Parse(textBox2.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Port is empty", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+            try
+            {
+                client.Connect(serverEndPoint);
+            }
+            catch(SocketException)
+            {
+                MessageBox.Show("Connection error", "Error", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            client.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void getAllAdsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sendMessage("get");
+        }
+
+        private void sendMessage(string message)
+        {
+
+            NetworkStream clientStream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+            Byte[] data = new Byte[3000];
+            int bytesCount = clientStream.Read(data, 0, data.Length);
+            string response = Encoding.UTF8.GetString(data, 0, bytesCount).Trim('\0');
+            textBox5.Text = response;
+
+        }
+
+        private void newAdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string title = textBox3.Text;
+            string body = textBox4.Text;
+            if (title.Equals("") || body.Equals(""))
+            {
+                MessageBox.Show("Field is empty", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            string message = "post " + title + " " + body;
+            sendMessage(message);
+        }
+    }
+}
---
+++ server/server/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        Thread listenThread;
+        static string workPath = Directory.GetCurrentDirectory() + "\\ads.txt";
+        myLogger logger = new myLogger(Directory.GetCurrentDirectory() + "\\log.txt");
+        static int countOfConnections = 0;
+        static bool workFlag = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+            Close();
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, 12000);
+
+        }
+
+        private void ListenClients()
+        {
+            listener.Start();
+
+            while (workFlag)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+
+            }
+
+        }
+
+        private void HandleClient(object client)
+        {
+
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+            //Socket socket = tcpClient.Client;
+            //IPEndPoint endpoint = (IPEndPoint)socket.RemoteEndPoint;
+            //IPAddress address = endpoint.Address;
+
+            byte[] message = new byte[5000];
+            int bytes;
+
+            while(true)
+            {
+                try
+                {
+                    bytes = clientStream.Read(message, 0, 5000);
+                    //залогировать
+                }
+                catch
+                {
+                    break;
+                }
+
+                string messageFromClient = Encoding.UTF8.GetString(message, 0, bytes).Trim('\0');
+                string[] msg = messageFromClient.Split(' ');
+                if (msg[0].Equals("GET"))
+                {
+                    string tempStr;
+                    string messageToClient = "";
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(workPath))
+                        {
+                            while ((tempStr = sr.ReadLine()) != null)
+                            {
+                                messageToClient += String.Concat(tempStr, "\r\n");
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        messageToClient = "Error";
+                    }
+                    Echo(messageToClient, clientStream);
+                }
+                else if (msg[0].Equals("POST"))
+                {
+                    string messageToClient = "";
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(workPath, true, System.Text.Encoding.Default))
+                        {
+                            sw.WriteLine(msg[1]);
+                            sw.WriteLine(msg[2]);
+                            sw.WriteLine("--------------------------------------------");
+                        }
+                        messageToClient = "Ad added";
+                    }
+                    catch
+                    {
+                        messageToClient = "Error";
+                    }
+                }
+
+            }
+            tcpClient.Close();
+
+        }
+
+        private void Echo(string message, NetworkStream clientStream)
+        {
+
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            clientStream.Write(buffer, 0, buffer.Length);
+            //залогировать количество отправленных байт
+            clientStream.Flush();
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            workFlag = false;
+            listener.Stop();
+            //залогировать
+        }
+    }
+}
